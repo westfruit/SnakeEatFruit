94288e0d0b3ac752f7a49e11c5c642bda0e8dd3a
郭旺成 <guo.wangcheng@vpclub.cn>
2017-09-26 17:56:28 +0800

    first commit

---
+++ Form_Snake.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form_Snake
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_Snake));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btn_start = new System.Windows.Forms.Button();
+            this.btn_pause = new System.Windows.Forms.Button();
+            this.btn_stop = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.statusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.cmb_foodspeed = new System.Windows.Forms.ComboBox();
+            this.cmb_bodyspeed = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.statusStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.panel1.Location = new System.Drawing.Point(5, 66);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(536, 344);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // btn_start
+            // 
+            this.btn_start.Location = new System.Drawing.Point(16, 17);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(75, 23);
+            this.btn_start.TabIndex = 1;
+            this.btn_start.TabStop = false;
+            this.btn_start.Text = "开始";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // btn_pause
+            // 
+            this.btn_pause.Enabled = false;
+            this.btn_pause.Location = new System.Drawing.Point(103, 17);
+            this.btn_pause.Name = "btn_pause";
+            this.btn_pause.Size = new System.Drawing.Size(75, 23);
+            this.btn_pause.TabIndex = 2;
+            this.btn_pause.TabStop = false;
+            this.btn_pause.Text = "暂停";
+            this.btn_pause.UseVisualStyleBackColor = true;
+            this.btn_pause.Click += new System.EventHandler(this.btn_pause_Click);
+            // 
+            // btn_stop
+            // 
+            this.btn_stop.Enabled = false;
+            this.btn_stop.Location = new System.Drawing.Point(185, 17);
+            this.btn_stop.Name = "btn_stop";
+            this.btn_stop.Size = new System.Drawing.Size(75, 23);
+            this.btn_stop.TabIndex = 3;
+            this.btn_stop.TabStop = false;
+            this.btn_stop.Text = "结束";
+            this.btn_stop.UseVisualStyleBackColor = true;
+            this.btn_stop.Click += new System.EventHandler(this.btn_stop_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 200;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 413);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(542, 22);
+            this.statusStrip1.TabIndex = 4;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(40, 17);
+            this.statusLabel.Text = "        ";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btn_stop);
+            this.groupBox1.Controls.Add(this.btn_pause);
+            this.groupBox1.Controls.Add(this.btn_start);
+            this.groupBox1.Location = new System.Drawing.Point(4, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(267, 50);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "操作";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.cmb_foodspeed);
+            this.groupBox2.Controls.Add(this.cmb_bodyspeed);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(280, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(258, 50);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "配置";
+            // 
+            // cmb_foodspeed
+            // 
+            this.cmb_foodspeed.FormattingEnabled = true;
+            this.cmb_foodspeed.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10"});
+            this.cmb_foodspeed.Location = new System.Drawing.Point(172, 14);
+            this.cmb_foodspeed.Name = "cmb_foodspeed";
+            this.cmb_foodspeed.Size = new System.Drawing.Size(66, 21);
+            this.cmb_foodspeed.TabIndex = 3;
+            this.cmb_foodspeed.Text = "6";
+            // 
+            // cmb_bodyspeed
+            // 
+            this.cmb_bodyspeed.FormattingEnabled = true;
+            this.cmb_bodyspeed.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10"});
+            this.cmb_bodyspeed.Location = new System.Drawing.Point(49, 14);
+            this.cmb_bodyspeed.Name = "cmb_bodyspeed";
+            this.cmb_bodyspeed.Size = new System.Drawing.Size(69, 21);
+            this.cmb_bodyspeed.TabIndex = 2;
+            this.cmb_bodyspeed.Text = "1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(124, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "随机：";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "移动：";
+            // 
+            // Form_Snake
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(542, 435);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.Name = "Form_Snake";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Snake Eat Food";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.Button btn_pause;
+        private System.Windows.Forms.Button btn_stop;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel statusLabel;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmb_foodspeed;
+        private System.Windows.Forms.ComboBox cmb_bodyspeed;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form_Snake.cs
@@ -0,0 +1,323 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+
+    public partial class Form_Snake : Form
+    {
+        public Form_Snake()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            ControlPaint.DrawGrid(e.Graphics, this.ClientRectangle, new Size(8, 8), Color.White);
+        }
+
+        Snake snake = null;
+        private void btn_start_Click(object sender, EventArgs e)
+        {
+            snake = new Snake(8, this.panel1, Over);
+            snake.Add(new Label());
+            snake.FoodSpeed = 2*Convert.ToInt32(this.cmb_foodspeed.Text);
+            timer1.Enabled = true;
+            timer1.Interval = 200 * Convert.ToInt32(this.cmb_bodyspeed.Text);
+            this.btn_start.Enabled = false;
+            this.btn_stop.Enabled = true;
+            this.btn_pause.Enabled = true;
+
+        }
+        public void Over()
+        {
+
+            this.timer1.Enabled = false;
+            this.btn_stop.Enabled = false;
+            this.btn_pause.Enabled = false;
+            this.btn_start.Enabled = true;
+            this.btn_pause.Text = "暂停";
+            this.statusLabel.Text = "Game Over";
+            Delay(2);
+            snake.Clear();
+            this.statusLabel.Text = "再来！";
+
+
+        }
+
+        public static bool Delay(int delayTime)
+        {
+            DateTime now = DateTime.Now;
+            int s;
+            do
+            {
+                TimeSpan spand = DateTime.Now - now;
+                s = spand.Seconds;
+                Application.DoEvents();
+            }
+            while (s < delayTime);
+            return true;
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)//取消方向键对控件的焦点的控件，用自己自定义的函数处理各个方向键的处理函数
+        {
+            switch (keyData)
+            {
+                case Keys.Up:
+                    if (Snake.Direction!=2)
+                    {
+                        Snake.Direction = 1;
+                    }
+                    
+                    return true;//不继续处理
+                case Keys.Down:
+                    if (Snake.Direction != 1)
+                    {
+                        Snake.Direction = 2;
+                    }
+                    return true;
+                case Keys.Left:
+                    if (Snake.Direction != 4)
+                    {
+                        Snake.Direction = 3;
+                    }
+                    return true;
+                case Keys.Right:
+                    if (Snake.Direction != 3)
+                    {
+                        Snake.Direction = 4;
+                    }
+                    return true;
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (snake != null)
+            {
+                snake.Move();
+                this.statusLabel.Text = string.Format("成绩：{0},随机：X={1},Y={2}", snake.Body.Count, snake.FoodPoint.X, snake.FoodPoint.Y);
+            }
+        }
+
+        private void btn_pause_Click(object sender, EventArgs e)
+        {
+            this.timer1.Enabled = !this.timer1.Enabled;
+            if (this.timer1.Enabled)
+            {
+                this.btn_pause.Text = "暂停";
+            }
+            else
+            {
+                this.btn_pause.Text = "继续";
+            }
+        }
+
+        private void btn_stop_Click(object sender, EventArgs e)
+        {
+            Over();
+        }
+
+
+    }
+
+    public delegate void OverDelegate();
+    public class Snake
+    {
+        public Snake(int between, Panel panel, OverDelegate overHandle)
+        {
+            this.Between = between;
+            this.DrawPanel = panel;
+            this.OverHandle = overHandle;
+
+        }
+
+        private Point _foodPoint = new Point(0, 0);
+        public Point FoodPoint { 
+            get { return _foodPoint; }
+            set { _foodPoint = value; }
+        }
+
+        public OverDelegate OverHandle { get; set; }
+
+        public Panel DrawPanel { get; set; }
+
+        //方格间距
+        public int Between { get; set; }
+
+        public Color BodyColor { get; set; }
+
+        /// <summary>
+        /// 1 up 2 down 3 left 4 right
+        /// </summary>
+        public static int Direction { get; set; }
+        public List<Control> Body { get; set; }
+
+        public void AddFood()
+        {
+            long tick = DateTime.Now.Ticks;
+            Random ran = new Random((int)(tick & 0xffffffffL) | (int)(tick >> 32));
+
+            if (ran.Next(100) % this.FoodSpeed == 0)
+            {
+                Label food = new Label();
+
+                int xRan = ran.Next(1, this.DrawPanel.Width / this.Between) * this.Between;
+                int yRan = ran.Next(1, this.DrawPanel.Height / this.Between) * this.Between;
+                FoodPoint = new Point(xRan, yRan);
+                var newFood = this.DrawPanel.GetChildAtPoint(FoodPoint);
+                if(newFood==null)
+                {
+                    food.Size = new Size(this.Between, this.Between);
+                    food.Location = new Point(xRan, yRan);
+                    food.BackColor = Color.Blue;
+                    food.BorderStyle = BorderStyle.FixedSingle;
+                    
+
+                    this.DrawPanel.Controls.Add(food);
+                }
+
+            }
+
+        }
+
+        public void Add(Label item)
+        {
+            item.BackColor = Color.Green;
+            item.Size = new Size(this.Between, this.Between);
+
+
+            if (Body == null)
+            {
+                Body = new List<Control>();
+                long tick = DateTime.Now.Ticks;
+                Random ran = new Random((int)(tick & 0xffffffffL) | (int)(tick >> 32));
+
+                int xRan = ran.Next(1,this.DrawPanel.Width / this.Between) * this.Between;
+                int yRan = ran.Next(1,this.DrawPanel.Height / this.Between) * this.Between;
+
+                item.Text = "*";
+                item.Location = new Point(xRan, yRan);
+                item.BackColor = Color.Red;
+                item.BorderStyle = BorderStyle.FixedSingle;
+                Snake.Direction = new Random().Next(1, 5);
+            }
+            else
+            {
+                item.Location = Body[Body.Count-1].Location;
+            }
+
+            this.Move();
+            this.DrawPanel.Controls.Add(item);
+
+            Body.Add(item);
+        }
+
+        public void Move()
+        {
+            AddFood();
+
+            if (this.Body == null || this.Body.Count < 1)
+            {
+                return;
+            }
+
+            Point oldPoint = new Point(0, 0);
+            Control food=null;
+            for (int i = 0; i < this.Body.Count; i++)
+            {
+                Point oldPoint2 = new Point(this.Body[i].Location.X, this.Body[i].Location.Y);
+
+                if (i==0)
+                {
+                    Point newPonit=new Point(0,0);
+
+                    if (Snake.Direction == 1)//up
+                    {
+                        newPonit = new Point(this.Body[0].Location.X, this.Body[0].Location.Y - this.Between);
+                    }
+                    else if (Snake.Direction == 2)//down
+                    {
+                        newPonit = new Point(this.Body[0].Location.X, this.Body[0].Location.Y + this.Between);
+                    }
+                    else if (Snake.Direction == 3)//left
+                    {
+                        newPonit = new Point(this.Body[0].Location.X - this.Between, this.Body[0].Location.Y);
+                    }
+                    else if (Snake.Direction == 4)//right
+                    {
+                         newPonit= new Point(this.Body[0].Location.X + this.Between, this.Body[0].Location.Y);
+                    }
+
+                    food = this.DrawPanel.GetChildAtPoint(newPonit);
+                    if (food!=null)
+                    {
+                        food.Location = new Point(-8, -8);
+                    }
+
+                    if (this.Body.Contains(food))
+                    {
+                        this.OverHandle();
+                        return;
+                    }
+
+                    if (newPonit.X < 0)
+                    {
+                        newPonit.X += this.DrawPanel.Width / this.Between * this.Between;
+                    }
+                    if (newPonit.Y < 0)
+                    {
+                        newPonit.Y += this.DrawPanel.Height / this.Between * this.Between;
+                    }
+                    if (newPonit.X > (this.DrawPanel.Width - this.Between))
+                    {
+                        newPonit.X = 0;
+                    }
+                    if (newPonit.Y > (this.DrawPanel.Height - this.Between))
+                    {
+                        newPonit.Y = 0;
+                    }
+
+                    //移动到新位置
+                    this.Body[0].Location = new Point(newPonit.X, newPonit.Y);
+
+                }
+                else
+                {
+                    this.Body[i].Location = new Point(oldPoint.X,oldPoint.Y);
+                }
+                
+                oldPoint =new Point(oldPoint2.X,oldPoint2.Y);
+
+            }//for end
+
+            if (food != null)
+            {
+                food.Location = new Point(oldPoint.X, oldPoint.Y);
+                food.BackColor = Color.Green;
+                this.Body.Add(food);
+            }
+        }
+
+        internal void Clear()
+        {
+            this.DrawPanel.Controls.Clear();
+        }
+
+        private int _foodSpeed = 5;
+        public int FoodSpeed { 
+            get { return _foodSpeed; } 
+            set { _foodSpeed=value; } 
+        }
+    }
+
+
+
+}
